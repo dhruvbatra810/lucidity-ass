95a8bb89f41309a27eddd7fa55a1782ba4568ebb
dhruv <dhruv@dhruvs-MacBook-Pro.local>
2026-02-15 16:10:12 +0530

    fix: deleted app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Navbar from './components/navbar'
-import './App.css'
 import Inventory from './features/dashboard/inventorySection'
 import { useEffect } from 'react'
 import { useAppDispatch } from './store/store'
